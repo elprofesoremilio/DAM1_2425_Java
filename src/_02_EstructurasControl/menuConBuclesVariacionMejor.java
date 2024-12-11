42a48d80d423b5062f2c92e228ab4509ecffc308
Emilio <elprofesoremilio@gmail.com>
2024-12-11 11:42:57 +0100

    Desde clase

--- src/_02_EstructurasControl/menuConBuclesVariacionMejor.java
+++ src/_02_EstructurasControl/menuConBuclesVariacionMejor.java
@@ -2,8 +2,10 @@ package _02_EstructurasControl;
 
 import java.util.Scanner;
 
-public class menuConBuclesVariacion {
-    public static void main(String[] args) {
+public class menuConBuclesVariacionMejor {
+    private static final Scanner sc = new Scanner(System.in);
+
+    public static int elegirOpcion() {
         String menu = "\n------------\n" +
                 "1. Sumar\n" +
                 "2. Restar\n" +
@@ -12,13 +14,14 @@ public class menuConBuclesVariacion {
                 "0. Terminar\n" +
                 "---------------\n" +
                 "Introduzca su elección: ";
+        System.out.print(menu);
+        return sc.nextInt();
+    }
 
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
 
-        int opcion = 3;
-        while (opcion!=0) {
-            System.out.print(menu);
-            opcion = sc.nextInt();
+        int opcion;
+        while ((opcion = elegirOpcion()) != 0) {
             switch (opcion) {
                 case 1:
                     System.out.println("SUMAR");
@@ -31,9 +34,6 @@ public class menuConBuclesVariacion {
                     break;
                 case 4:
                     System.out.println("DIVIDIR");
-                    break;
-                case 0:
-                    System.out.println("Adiós");
                     break;
                 default:
                     System.out.println("Opción incorrecta");
