710a9430b876909fd044840b9da5f5795a0ef972
Emilio <elprofesoremilio@gmail.com>
2025-01-24 13:31:56 +0100

    Desde clase

--- src/_02_ClasesGenerales/C_ArrayList.java
+++ src/_02_ClasesGenerales/C_ArrayList.java
@@ -50,6 +50,37 @@ public class C_ArrayList {
         datos = nuevosDatos;
     }
 
+    public void remove(int index) {
+        int[] nuevosDatos = new int[longitud-1];
+        for (int i=0; i<index; i++) {
+            nuevosDatos[i] = datos[i];
+        }
+        for (int i=index; i<nuevosDatos.length; i++) {
+            nuevosDatos[i] = datos[i+1];
+        }
+        datos = nuevosDatos;
+        longitud--;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+        if (longitud>1) {
+            result.append("[ ");
+            result.append(datos[0]);
+            for (int i = 1; i < datos.length; i++) {
+                result.append(", ");
+                result.append(datos[i]);
+            }
+            result.append(" ]");
+        } else if (longitud==1) {
+            result.append(String.format("[ %d ]",datos[0]));
+        } else {
+            result.append("[ ]");
+        }
+        return result.toString();
+    }
+
     public static void main(String[] args) {
         C_ArrayList matriz = new C_ArrayList(5);
         Random r = new Random();
@@ -57,32 +88,28 @@ public class C_ArrayList {
             matriz.setDato(i,r.nextInt(11)); // matriz[i] = r.nextInt(11);
         }
 
-        System.out.print("[");
-        for (int i=0; i<matriz.size(); i++) {
-            System.out.print(" "+matriz.getDato(i));
-        }
-        System.out.println(" ]");
+        System.out.println(matriz);
 
         matriz.setSize(7);
         matriz.setDato(matriz.size()-1, 88);
         matriz.add(77);
 
-        System.out.print("[");
-        for (int i=0; i<matriz.size(); i++) {
-            System.out.print(" "+matriz.getDato(i));
-        }
-        System.out.println(" ]");
+        System.out.println(matriz);
+
 
         matriz.add(2, 33);
         matriz.add(0, 0);
         matriz.add(matriz.size()-1, 99);
         matriz.add(matriz.size(), 99);
 
+        System.out.println(matriz);
 
-        System.out.print("[");
-        for (int i=0; i<matriz.size(); i++) {
-            System.out.print(" "+matriz.getDato(i));
-        }
-        System.out.println(" ]");
+        matriz.remove(3);
+        System.out.println(matriz);
+        matriz.remove(0);
+        System.out.println(matriz);
+        matriz.remove(matriz.size()-1);
+        System.out.println(matriz);
+
     }
 }
