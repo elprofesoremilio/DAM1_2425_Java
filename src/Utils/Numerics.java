a07321cecad8df3a632acf9e0425f996a35a7850
Emilio <elprofesoremilio@gmail.com>
2025-01-27 13:50:14 +0100

    Desde clase

--- src/Utils/Numerics.java
+++ src/Utils/Numerics.java
@@ -2,9 +2,20 @@ package Utils;
 
 public class Numerics {
     public static boolean isPrime(int number) {
-        boolean result = true;
-        // TODO demostrar si no lo es
+        int half = number/2 + 1;
+        for (int i = 2; i <= half; i++) {
+            if (number%i==0) {
+                return false;
+            }
+        }
+        return true;
+    }
 
-        return result;
+    public static void main(String[] args) {
+        System.out.println(isPrime(341));  // debería dar false
+        System.out.println(isPrime(3413));  // debería dar true
+        System.out.println(isPrime(49));  // debería dar false
+        System.out.println(isPrime(961));  // debería dar false
+        System.out.println(isPrime(967));  // debería dar true
     }
 }
