b87ccf691a77fa5851acd93cb2a47c50bf0429c2
Emilio <elprofesoremilio@gmail.com>
2025-01-27 13:34:11 +0100

    Desde clase

---
+++ src/Utils/Numerics.java
@@ -0,0 +1,10 @@
+package Utils;
+
+public class Numerics {
+    public static boolean isPrime(int number) {
+        boolean result = true;
+        // TODO demostrar si no lo es
+
+        return result;
+    }
+}
