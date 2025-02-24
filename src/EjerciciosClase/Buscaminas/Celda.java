378932ce7b14c736bd691dcb9e9c942a09436089
Emilio <elprofesoremilio@gmail.com>
2025-02-24 14:26:09 +0100

    Desde clase

---
+++ src/EjerciciosClase/Buscaminas/Celda.java
@@ -0,0 +1,52 @@
+package EjerciciosClase.Buscaminas;
+
+public class Celda {
+    private boolean mina;
+    private byte minasAlrededor;
+    private boolean destapada;
+
+    public Celda() {
+        mina = false;
+        minasAlrededor = 0;
+        destapada = false;
+    }
+
+    public boolean destapar() {
+        destapada = true;
+        return mina;
+    }
+
+    public void setMinasAlrededor(byte minasAlrededor) {
+        this.minasAlrededor = minasAlrededor;
+    }
+
+    public byte getMinasAlrededor() {
+        return minasAlrededor;
+    }
+
+    public boolean isMina() {
+        return mina;
+    }
+
+    public void colocarMina() {
+        mina = true;
+    }
+
+    @Override
+    public String toString() {
+        if (mina) {
+            return "*";
+        } else {
+            return Byte.toString(minasAlrededor);
+        }
+    }
+
+    public static void main(String[] args) {
+        Celda celda = new Celda();
+        celda.colocarMina();
+        System.out.println(celda);
+        celda.destapar();
+        System.out.println(celda.destapada);
+    }
+
+}
